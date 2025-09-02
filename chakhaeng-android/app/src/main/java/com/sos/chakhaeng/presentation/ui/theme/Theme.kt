1dac417d4d748143bc13ef48ead74b269b2fc688
박상윤 <park48569803@gmail.com>
2025-09-02 12:51:42 +0900

    chore: Type setting
    Type setting

--- chakhaeng-android/app/src/main/java/com/sos/chakhaeng/presentation/ui/theme/Theme.kt
+++ chakhaeng-android/app/src/main/java/com/sos/chakhaeng/presentation/ui/theme/Theme.kt
@@ -255,7 +255,7 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun AppTheme(
+fun ChakHaengTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -273,7 +273,7 @@ fun AppTheme(
 
   MaterialTheme(
     colorScheme = colorScheme,
-    typography = AppTypography,
+    typography = chakhaengTypography(),
     content = content
   )
 }
