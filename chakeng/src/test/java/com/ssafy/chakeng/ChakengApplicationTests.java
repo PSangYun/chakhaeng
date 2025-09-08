9f105e9772bc470684aa13bd74b7a6745186eb22
박성준 <sungung1026@naver.com>
2025-09-08 10:36:05 +0900

    feat : first init

---
+++ chakeng/src/test/java/com/ssafy/chakeng/ChakengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.chakeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChakengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
