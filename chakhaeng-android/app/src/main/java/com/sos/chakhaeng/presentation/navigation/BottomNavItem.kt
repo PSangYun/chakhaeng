a783efa586833a8ce3c1fd1d760f86cc069323f7
park99999 <sungung1026@naver.com>
2025-09-15 15:03:08 +0900

    feat : 홈화면 UI 수정

--- chakhaeng-android/app/src/main/java/com/sos/chakhaeng/presentation/navigation/BottomNavItem.kt
+++ chakhaeng-android/app/src/main/java/com/sos/chakhaeng/presentation/navigation/BottomNavItem.kt
@@ -1,39 +1,37 @@
 package com.sos.chakhaeng.presentation.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.ui.graphics.vector.ImageVector
+import com.sos.chakhaeng.R
 
 data class BottomNavItem(
     val route: String,
-    val icon: ImageVector,
+    val icon: Int,
     val label: String
 )
 
 val bottomNavItems = listOf(
     BottomNavItem(
         route = Routes.Home.route,
-        icon = Icons.Default.Home,
+        icon = R.drawable.home,
         label = "홈"
     ),
     BottomNavItem(
         route = Routes.Detection.route,
-        icon = Icons.Default.PhotoCamera,
+        icon = R.drawable.cam,
         label = "탐지"
     ),
     BottomNavItem(
         route = Routes.Report.route,
-        icon = Icons.Default.Report,
+        icon = R.drawable.report,
         label = "신고"
     ),
     BottomNavItem(
         route = Routes.Statistics.route,
-        icon = Icons.Default.BarChart,
+        icon = R.drawable.stats,
         label = "통계"
     ),
     BottomNavItem(
         route = Routes.Profile.route,
-        icon = Icons.Default.Person,
+        icon = R.drawable.profile,
         label = "프로필"
     )
 )
