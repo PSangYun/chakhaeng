8e3481be8655a80948547c4604d5723511f2e1d6
서현호 <shh2849@naver.com>
2025-09-19 15:48:37 +0900

    fix: conflict resoloved

--- chakhaeng-android/app/src/main/java/com/sos/chakhaeng/presentation/ui/screen/statistics/StatisticsScreen.kt
+++ chakhaeng-android/app/src/main/java/com/sos/chakhaeng/presentation/ui/screen/statistics/StatisticsScreen.kt
@@ -7,8 +7,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sos.chakhaeng.presentation.theme.BackgroundGray
 import com.sos.chakhaeng.presentation.theme.chakhaengTypography
 import com.sos.chakhaeng.presentation.ui.components.statistics.section.ErrorSection
@@ -18,22 +16,15 @@ import com.sos.chakhaeng.presentation.ui.components.statistics.StatisticsContent
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StatisticsScreen(
-    statisticsViewModel: StatisticsViewModel= hiltViewModel(),
-    paddingValues: PaddingValues
+    uiState: StatisticsUiState,
+    statisticsViewModel: StatisticsViewModel
 ) {
-    val uiState by statisticsViewModel.uiState.collectAsStateWithLifecycle()
 
-    uiState.error?.let { error ->
-        LaunchedEffect(error) {
-            statisticsViewModel.clearError()
-        }
-    }
-     val bottomPadding = paddingValues.calculateBottomPadding()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(BackgroundGray)
-            .padding(bottom = bottomPadding)
     ) {
         CenterAlignedTopAppBar(
             title = {
@@ -45,19 +36,22 @@ fun StatisticsScreen(
             },
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = Color.White
-            )
+            ),
+            windowInsets = WindowInsets(0)
         )
 
         when {
             uiState.isLoading -> {
                 LoadingSection()
             }
+
             uiState.error != null -> {
                 ErrorSection(
-                    error = uiState.error!!,
+                    error = uiState.error,
                     onRetry = { statisticsViewModel.refreshStatistics() }
                 )
             }
+
             else -> {
                 StatisticsContent(
                     uiState = uiState,
